39496e17de76138d3d226c8e9363949b4d60d138
Ana Caroline Vasconcellos <124820588+Vasconcellos-cipher@users.noreply.github.com>
2026-08-27 21:14:16 +0000

    confim

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ pygame.init()
 WIDTH = 850
 HEIGHT = 530
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Sonic Race Remastered")
+
+bg_path = os.path.join("assets", "img", "background.png")
+background = pygame.image.load(bg_path).convert()
+background = pygame.transform.scale(background, (WIDTH, HEIGHT))
+bg_x = 0
+bg_speed = 3
 
 clock = pygame.time.Clock()
 
@@ -19,6 +26,13 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             
+    bg_x -= bg_speed
+    if bg_x <= -WIDTH:
+        bg_x = 0
+        
+    screen.blit(background, (bg_x, 0))
+    screen.blit(background, (bg_x + WIDTH, 0))
+        
     pygame.display.flip() 
             
 pygame.quit()
